af474d24bbee1fe0c2f6cde44eac9c4e58c876b4
Young <ylee546@wisc.edu>
2017-11-07 19:56:37 -0600

    Remove viewer from __init__

--- specdal/__init__.py
+++ specdal/__init__.py
@@ -1,6 +1,5 @@
 from .spectrum import Spectrum
 from .collection import Collection, df_to_collection, proximal_join
 from .reader import read
-from .gui.viewer import main as viewer
 
 # __all__ = ['spectrum', 'collection', 'reader']
