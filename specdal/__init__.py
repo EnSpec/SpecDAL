a0e8e85a8d0e978ef4576d722b150570eb4c16e9
westphallm1 <westphallm1@protonmail.com>
2018-03-23 14:16:41 -0500

    fixed specdal/__init__.py to import along new paths

--- specdal/__init__.py
+++ specdal/__init__.py
@@ -1,5 +1,5 @@
-from .spectrum import Spectrum
-from .collection import Collection, df_to_collection, proximal_join
+from .containers.spectrum import Spectrum
+from .containers.collection import Collection, df_to_collection, proximal_join
 from .readers import read
 
 # __all__ = ['spectrum', 'collection', 'reader']
