f15b2040da2317dd77acbc253fb7101e1d4abf2f
westphallm1 <westphallm1@protonmail.com>
2018-03-23 15:13:11 -0500

    changed version on setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='specdal',
-      version='0.2.2.dev0',
+      version='0.2.2',
       description='Package for processing spectroscopy data',
       long_description=open('README.rst').read(),
       scripts=[
